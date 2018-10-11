ae01ac840fe27107a6a3ecec564a51a84b7b5aba
xxxmatko <martin.sutka@gmail.com>
2018-10-11 07:26:17 +0200

    Grunt file

---
+++ gruntfile.js
@@ -0,0 +1,158 @@
+module.exports = function (grunt) {
+    //#region [ Config ]
+
+    grunt.initConfig({
+        package: grunt.file.readJSON("package.json"),
+        clean: {
+            wwwroot: [
+                "wwwroot/**/*"
+            ]
+        },
+        copy: {
+            index: {
+                options: {
+                    process: function (content, srcpath) {
+                        return content
+                            .replace(/\{version\}/g, grunt.config("package").version)
+                            .replace(/\{homepage\}/g, grunt.config("package").homepage)
+                            .replace(/\{cacheBust\}/g, new Date().getTime());
+                    }
+                },
+                files: [{
+                    expand: true,
+                    src: ["index.xhtml"],
+                    rename: function () {
+                        return "wwwroot/index.html";
+                    }
+                }]
+            },
+            materialize: {
+                options: {
+                    process: function (content, srcpath) {
+                        return "(function (root, factory) {\n\
+                                    if (typeof (define) === 'function' && define.amd) {\n\
+                                        define(['jquery'], factory);\n\
+                                    }\n\
+                                    else {\n\
+                                        factory(root.jQuery);\n\
+                                    }\n\
+                                }(typeof (self) !== 'undefined' ? self : this, function (jQuery) {\n"
+                                    + content +
+                                    "return M;\n\
+                                }));";
+                    }
+                },
+                files: [{
+                    expand: true,
+                    cwd: "js/",
+                    src: ["**/materialize.js"],
+                    dest: "wwwroot/js/",
+                    filter: "isFile"
+                }]
+            },
+            js: {
+                files: [{
+                    expand: true,
+                    cwd: "js/",
+                    src: ["**", "!**/*.less", "!**/materialize.js"],
+                    dest: "wwwroot/js/",
+                    filter: "isFile"
+                }]
+            },
+            css: {
+                files: [{
+                    expand: true,
+                    cwd: "css/",
+                    src: ["**"],
+                    dest: "wwwroot/css/",
+                    filter: "isFile"
+                }]
+            }
+        },
+        less: {
+            options: {
+                paths: ["less"],
+                strictMath: false
+            },
+            src: {
+                files: {
+                    "wwwroot/css/site.css": "less/site.less",
+                    "wwwroot/css/app.css": "js/components/app/app.less"
+                }
+            }
+        },
+        jshint: {
+            options: {
+                debug: true,
+                multistr: true,
+                sub: true,
+                laxbreak: true,
+                globals: {
+                    jQuery: true
+                }
+            },
+            src: [
+                "gruntfile.js",
+                "js/**/*.js",
+                "!js/libs/*.js"
+            ]
+        }
+    });
+
+    //#endregion
+
+
+    //#region [ Tasks ]
+
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+
+    grunt.registerTask("cleanTask", cleanTask);
+    grunt.registerTask("buildTask", buildTask);
+
+    //#endregion
+
+
+    //#region [ Methods ]
+
+    /**
+     * Clean task.
+     */
+    function cleanTask() {
+        // List of tasks
+        var tasks = [
+            "clean:wwwroot"
+        ];
+
+
+        // Run tasks
+        grunt.task.run.apply(grunt.task, tasks);
+    }
+
+
+    /**
+     * Build task.
+     */
+    function buildTask(target) {
+        var version = grunt.config("package").version;
+
+        grunt.log.writeln("Building version \"" + version + "\"");
+
+        // List of tasks
+        var tasks = [
+            "copy:index",
+            "copy:materialize",
+            "copy:js",
+            "copy:css",
+            "less",
+            "jshint"
+        ];
+
+        // Run tasks
+        grunt.task.run.apply(grunt.task, tasks);
+    }    
+
+    //#endregion
+};
